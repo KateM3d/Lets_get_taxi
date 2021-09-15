faaed7e6fbf9a3321c44c54e4943edec14e3e1c2
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-15 19:17:43 -0400

    added validation for inputs

--- script.js
+++ script.js
@@ -12,23 +12,43 @@ button.addEventListener('click', calcAmount);
 
 function calcAmount(e) {
     e.preventDefault();
-    const bill = document.querySelector('#bill').value;
-    const people = document.querySelector('#passengers').value;
-    const tip = document.querySelector('#tip').value;
-    if (bill.trim() === "" || people.trim() === "" || people < 1) {
+    let bill = document.querySelector('#bill');
+    let people = document.querySelector('#passengers');
+    let tip = document.querySelector('#tip').value;
+    // if (bill.trim() === "" || people.trim() === "" || people < 1) {
+    //     Swal.fire({
+    //         icon: 'error',
+    //         title: 'Oops...',
+    //         text: 'Please enter number!',
+
+    //     })
+    // }
+
+    if (isValidNumber(bill.value) === false || isValidNumber(people.value) === false || people.value < 1) {
+
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Please enter number!',
 
         })
+        bill.value = '';
+        people.value = '';
+
+    } else {
+        let amountPerPerson = bill.value / people.value;
+        let tipPerPerson = (bill.value * tip) / people.value;
+        let totalPerPerson = amountPerPerson + tipPerPerson;
+
+        document.querySelector('#divBill').textContent = amountPerPerson.toFixed(2);
+        document.querySelector('#divTip').textContent = tipPerPerson.toFixed(2);
+        document.querySelector('#divTotal').textContent = totalPerPerson.toFixed(2);
     }
+}
 
-    let amountPerPerson = bill / people;
-    let tipPerPerson = (bill * tip) / people;
-    let totalPerPerson = amountPerPerson + tipPerPerson;
 
-    document.querySelector('#divBill').textContent = amountPerPerson.toFixed(2);
-    document.querySelector('#divTip').textContent = tipPerPerson.toFixed(2);
-    document.querySelector('#divTotal').textContent = totalPerPerson.toFixed(2);
+function isValidNumber(value) {
+    let isNotEmpty = value.trim().length !== 0;
+    let isNumber = !Number.isNaN(Number(value));
+    return isNotEmpty && isNumber;
 }
