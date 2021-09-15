1d248bf4d704c08ff9e99d710594a9541947ed3c
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-15 19:35:10 -0400

    added animation on header and showTotalAmount container

---
+++ animation.js
@@ -0,0 +1,5 @@
+gsap.to(".headder", { duration: 3, rotationY: 360 });
+
+gsap.from("#billSplit", { delay: 1, duration: 1, x: 400, opacity: 0 });
+gsap.from("#totalTip", { delay: 2, duration: 1, x: 400, opacity: 0 });
+gsap.from("#totalAmount", { delay: 3, duration: 1, x: 400, opacity: 0 });
